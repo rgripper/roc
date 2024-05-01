bf779290aeff8de23169bec67ee635e1386bcdae
Luke Boswell <lukewilliamboswell@gmail.com>
2024-05-01 17:13:58 +1000

    remove host rebuilding

--- crates/cli/tests/cli_run.rs
+++ crates/cli/tests/cli_run.rs
@@ -54,8 +54,7 @@ mod cli_run {
     const OPTIMIZE_FLAG: &str = concatcp!("--", roc_cli::FLAG_OPTIMIZE);
     const LINKER_FLAG: &str = concatcp!("--", roc_cli::FLAG_LINKER);
     const CHECK_FLAG: &str = concatcp!("--", roc_cli::FLAG_CHECK);
-    #[allow(dead_code)]
-    const PREBUILT_PLATFORM: &str = concatcp!("--", roc_cli::FLAG_PREBUILT);
+
     #[allow(dead_code)]
     const TARGET_FLAG: &str = concatcp!("--", roc_cli::FLAG_TARGET);
 
@@ -1034,7 +1033,7 @@ mod cli_run {
         use cli_utils::helpers::cli_testing_dir;
 
         #[allow(unused_imports)]
-        use super::{check_output_with_stdin, OPTIMIZE_FLAG, PREBUILT_PLATFORM};
+        use super::{check_output_with_stdin, OPTIMIZE_FLAG};
 
         #[allow(unused_imports)]
         use std::{path::Path, sync::Once};
@@ -1112,7 +1111,7 @@ mod cli_run {
                 check_output_with_stdin(
                     file_name,
                     stdin,
-                    &[PREBUILT_PLATFORM],
+                    &[],
                     &[],
                     &[],
                     expected_ending,
@@ -1124,7 +1123,7 @@ mod cli_run {
             check_output_with_stdin(
                 file_name,
                 stdin,
-                &[PREBUILT_PLATFORM, OPTIMIZE_FLAG],
+                &[OPTIMIZE_FLAG],
                 &[],
                 &[],
                 expected_ending,
